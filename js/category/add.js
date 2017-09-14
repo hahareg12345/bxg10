88691259eb4eb98f2021edc4888e480a3885b4bb
ddd <ddd@qq.com>
2017-09-14 16:09:52 +0800

    添加分类渲染上级分类数据

--- js/category/add.js
+++ js/category/add.js
@@ -7,20 +7,36 @@ define([
     "jquery",
     "text!tpls/categoryAdd.html",       //添加分类模态框模板
     "template"
-],function($,categoryAddTpl,template){
+], function ($, categoryAddTpl, template) {
 
-    return function(){
+    return function () {
 
-        //1、编译模板
-        var html=template.render(categoryAddTpl,{});
 
-        //2、把内容放到页面中
-        var $html=$(html)
-            .appendTo("body").modal()
-            .on("hidden.bs.modal",function(){
+        $.ajax({
+            url: "/api/category/top",
+            type: "get",
+            success: function (res) {
 
-                $html.remove();
-            });
+                //0、在数组的头部：预置顶级分类的数据
+                res.result.unshift({
+                    cg_id:0,
+                    cg_name:"顶级分类"
+                });
+
+                //1、编译模板
+                var html = template.render(categoryAddTpl, res);
+
+                //2、把内容放到页面中
+                var $html = $(html)
+                    .appendTo("body").modal()
+                    .on("hidden.bs.modal", function () {
+
+                        $html.remove();
+                    });
+
+            }
+        })
+
 
     }
 })
