1016a9665b06ab813951033e0fb82fb4189f40e0
ddd <ddd@qq.com>
2017-09-15 10:46:56 +0800

    完成课程列表数据渲染

--- js/course/list.js
+++ js/course/list.js
@@ -11,9 +11,17 @@ define([
 
 
     return function(){
-        //1、编译模板
-        var html=template.render(courseListTpl,{});
-        //2、把内容放到页面中的指定位置
-        $(".main .content-container").html(html);
+        //$.get专门用于get请求：   类似的：$.post
+        //参数1：url
+        //参数2：data
+        //参数3：success
+        $.get("/api/course",{},function(res){
+            if(res.code!=200) throw new Error(res.msg);
+
+            //编译模板
+            var html=template.render(courseListTpl,res);
+
+            $(".main .content-container").html(html);
+        })
     }
 })
