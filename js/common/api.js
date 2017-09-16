877f89a52cec189cbc6b08f4f575b3845d9e605c
ddd <ddd@qq.com>
2017-09-16 10:43:21 +0800

    封装api模块ajax方法

---
+++ js/common/api.js
@@ -0,0 +1,43 @@
+/**
+ * 处理通用的ajax请求
+ * Author:Wilbert
+ *   Date:2017/9/16
+ */
+define([
+    "jquery"
+], function ($) {
+
+
+    return {
+        /**
+         *
+         * @param url
+         * @param type
+         * @param data
+         * @param callback 接受了匿名函数的内存地址
+         */
+        ajax: function (url, type, data, callback) {
+            $.ajax({
+                url: "/api/"+url,
+                type: type,
+                data: data,
+                success: function (result) {
+                    if (result.code != 200) throw new Error(result.msg);
+
+
+                    callback(result);
+                },
+                error: function (res) {
+                    throw new Error(res);
+                }
+            });
+        }
+    }
+});
+// //main.js
+// require(["common/api"], function (api) {
+//
+//     api.ajax("teacher", "get", {},function (res) {
+//         //这里的形参res，接受了callback调用时候，传入的实参
+//     });
+// })
--- js/teacher/list.js
+++ js/teacher/list.js
@@ -9,8 +9,9 @@ define([
     "template",                    //arttemplate模板引擎
     "teacher/show",    //查看讲师模块              //"./show"是正确答案      //"show"是错误答案
     "teacher/add",      //添加讲师模块
-    "teacher/edit"      //编辑讲师模块
-], function ($, teacherListTpl, template, teacherShow, teacherAdd,teacherEdit) {
+    "teacher/edit",      //编辑讲师模块
+    "common/api"        //通过api模块-->处理ajax请求
+], function ($, teacherListTpl, template, teacherShow, teacherAdd,teacherEdit,api) {
 
 
     //把模块的主要业务逻辑写在返回值中
@@ -19,83 +20,79 @@ define([
 
         var fn = arguments.callee;
 
-        $.ajax({
-            url: "/api/teacher",
-            type: "get",
-            success: function (res) {
-                if (res.code != 200) throw new Error(res.msg);
+        api.ajax("teacher","get",{},function (res) {
 
-                //数据成功获取-->把数据渲染到模板中-->arttemplate模板引擎编译模板内容
-                var html = template.render(teacherListTpl, res);
-                //把编译之后的结果放到页面中的指定位置
-                $(".main .content-container").html(html);
+            //数据成功获取-->把数据渲染到模板中-->arttemplate模板引擎编译模板内容
+            var html = template.render(teacherListTpl, res);
+            //把编译之后的结果放到页面中的指定位置
+            $(".main .content-container").html(html);
 
-                //给查看按钮绑定事件
-                $("div.panel")
-                    //查看讲师
-                    .on("click", ".btn-show", function () {
-                        //this:查看按钮
+            //给查看按钮绑定事件
+            $("div.panel")
+            //查看讲师
+                .on("click", ".btn-show", function () {
+                    //this:查看按钮
 
-                        var tc_id = $(this).parent().attr("tc_id");
+                    var tc_id = $(this).parent().attr("tc_id");
 
 
-                        //调用查看讲师模块的代码
-                        teacherShow(tc_id);
-                    })
-                    //添加讲师
-                    .on("click", ".btn-add", function () {
-                        //this:添加讲师按钮
+                    //调用查看讲师模块的代码
+                    teacherShow(tc_id);
+                })
+                //添加讲师
+                .on("click", ".btn-add", function () {
+                    //this:添加讲师按钮
 
-                        //调用添加讲师模块的代码
-                        teacherAdd(function () {
+                    //调用添加讲师模块的代码
+                    teacherAdd(function () {
 
 
-                            //刷新讲师列表？
-                            //fn();
+                        //刷新讲师列表？
+                        //fn();
 
-                            fn3();
-                        });
+                        fn3();
+                    });
 
-                    })
-                    //启用注销讲师
-                    .on("click",".btn-status",function(){
+                })
+                //启用注销讲师
+                .on("click",".btn-status",function(){
 
-                        var $btnStatus=$(this);
+                    var $btnStatus=$(this);
 
-                        $.ajax({
-                            url:"/api/teacher/handle",
-                            type:"post",
-                            data:{
-                                tc_id:$(this).parent().attr("tc_id"),
-                                tc_status:$(this).parent().attr("tc_status")
-                            },
-                            success:function(res){
-                                if(res.code!=200) throw new Error(res.msg);
+                    $.ajax({
+                        url:"/api/teacher/handle",
+                        type:"post",
+                        data:{
+                            tc_id:$(this).parent().attr("tc_id"),
+                            tc_status:$(this).parent().attr("tc_status")
+                        },
+                        success:function(res){
+                            if(res.code!=200) throw new Error(res.msg);
 
-                                //已经成功的修改了服务器中的数据
-                                //-->修改显示的文本
+                            //已经成功的修改了服务器中的数据
+                            //-->修改显示的文本
 
-                                var status=res.result.tc_status;//用户最新的状态值
+                            var status=res.result.tc_status;//用户最新的状态值
 
-                                //a、修改状态列文本
-                                $btnStatus.parent().siblings(".td-status").text(status==0?"启用":"注销");
-                                //b、修改用户状态按钮值
-                                $btnStatus.text(status==0?"注销":"启用");
-                                //c、修改父元素中的tc_status属性值
-                                $btnStatus.parent().attr("tc_status",status);
-                            }
-                        })
-
-                    })
-                    //编辑讲师
-                    .on("click",".btn-edit",function(){
-                        var tc_id=$(this).parent().attr("tc_id");
-
-                        teacherEdit(tc_id);
+                            //a、修改状态列文本
+                            $btnStatus.parent().siblings(".td-status").text(status==0?"启用":"注销");
+                            //b、修改用户状态按钮值
+                            $btnStatus.text(status==0?"注销":"启用");
+                            //c、修改父元素中的tc_status属性值
+                            $btnStatus.parent().attr("tc_status",status);
+                        }
                     })
 
+                })
+                //编辑讲师
+                .on("click",".btn-edit",function(){
+                    var tc_id=$(this).parent().attr("tc_id");
 
-            }
+                    teacherEdit(tc_id);
+                })
+
+
         })
+
     }
 })
